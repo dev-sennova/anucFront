6f42a3bb07b58c996d470bd4fc5f706754e56d62
SavitarADSO <145699111+SavitarADSO@users.noreply.github.com>
2024-07-29 10:57:47 -0500

    Arreglos en las gráficas(Vista administrador) (#6)

--- src/app/components/administrador/administrador.component.ts
+++ src/app/components/administrador/administrador.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-administrador',
@@ -17,6 +17,25 @@ export class AdministradorComponent {
     document.getElementById('menu')!.classList.remove('open');
     document.getElementById('overlay')!.classList.remove('active');
     document.getElementById('menu-btn')!.classList.remove('hidden');
+  }
+
+  toggleProfileMenu() {
+    const profileMenu = document.getElementById('profile-menu');
+    if (profileMenu) {
+      profileMenu.classList.toggle('active');
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: MouseEvent) {
+    const profileMenu = document.getElementById('profile-menu');
+    const profileIcon = document.getElementById('profile_icon');
+    if (profileMenu && profileIcon) {
+      const target = event.target as HTMLElement;
+      if (!profileMenu.contains(target) && !profileIcon.contains(target)) {
+        profileMenu.classList.remove('active');
+      }
+    }
   }
 
 }
