2d8ae5887a9149831a4c68bab83148ed3c54dbce
SavitarADSO <pee7491@gmail.com>
2024-08-26 13:46:34 -0500

    Rutas protegidas

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -7,17 +7,13 @@ export const roleGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: R
   const loginService = inject(LoginService);
   const router = inject(Router);
 
-  // Obtén los roles esperados desde los datos de la ruta
   const expectedRoles = route.data['roles'] as Array<string>;
   
-  // Obtén el rol del usuario desde el servicio de login o desde el localStorage
   const userRole = localStorage.getItem('rol_usuario');
 
   if (userRole && expectedRoles.includes(userRole)) {
-    // Si el rol del usuario está en la lista de roles permitidos, permite el acceso
     return true;
   } else {
-    // Si no, redirige al usuario a la ruta de inicio según su rol
     switch (userRole) {
       case 'Administrador':
         router.navigate(['administrador/inicio']);
@@ -25,7 +21,6 @@ export const roleGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: R
       case 'Asociado':
         router.navigate(['asociado/inicio-asociado']);
         break;
-      // Puedes añadir más casos según los roles que manejes
       default:
         router.navigate(['home']);
         break;
