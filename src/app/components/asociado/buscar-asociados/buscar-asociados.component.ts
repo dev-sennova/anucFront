023032008cf41293b4648c3e6ab51c5ab23b3cce
SavitarADSO <pee7491@gmail.com>
2024-10-23 18:42:19 -0500

    Implemetaciones Miguel 23 oct...

--- src/app/components/asociado/buscar-asociados/buscar-asociados.component.ts
+++ src/app/components/asociado/buscar-asociados/buscar-asociados.component.ts
@@ -15,9 +15,26 @@ export class BuscarAsociadosComponent implements OnInit {
   constructor(private listadosService: ListadosService) {}
 
   ngOnInit(): void {
-    this.listadosService.getAsociadosFinca().subscribe((data: any) => {
-      this.asociados = data;
-      this.asociadosFiltrados = [...this.asociados]; // Inicialmente muestra todos
+    this.listadosService.getAsociadosFinca().subscribe((data: any[]) => {
+    
+
+      this.asociados = data.filter(asociado => 
+        asociado.habeasData !== 0 &&
+        (asociado.telefono || asociado.correo || asociado.whatsapp || asociado.facebook || asociado.instagram) 
+      );
+      
+      this.asociadosFiltrados = this.asociados.map(asociado => ({
+        ...asociado,
+        telefono: (asociado.permisotelefono === 1 || asociado.permisotelefono === 3) ? asociado.telefono : null,
+        correo: (asociado.permisocorreo === 1 || asociado.permisocorreo === 3) ? asociado.correo : null,
+        whatsapp: (asociado.permisowhatsapp === 1 || asociado.permisowhatsapp === 3) ? asociado.whatsapp : null,
+        facebook: (asociado.permisofacebook === 1 || asociado.permisofacebook === 3) ? asociado.facebook : null,
+        instagram: (asociado.permisoinstagram === 1 || asociado.permisoinstagram === 3) ? asociado.instagram : null,
+      })).filter(asociado => 
+        asociado.telefono || asociado.correo || asociado.whatsapp || asociado.facebook || asociado.instagram 
+      );
+
+      this.asociadosFiltrados = [...this.asociadosFiltrados]; 
     });
   }
 
