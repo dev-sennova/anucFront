090f536e5b261775ec5ffa072c7ccfb21d628399
Yesenia <yesego02@gmail.com>
2024-11-05 16:09:41 -0500

    Fases en categorias

--- src/app/components/asociado/categoria/categoria.component.ts
+++ src/app/components/asociado/categoria/categoria.component.ts
@@ -29,16 +29,50 @@ export class CategoriaComponent implements OnInit {
   productos: any[] = [];
   medidas: any[] = [];
   isModalOpen: boolean = false;
+  mostrarContenido: boolean = false;
+  gruposF: any[] = []; 
+  categoriaId: number; 
 
+  iconosFase1 = [
+    'assets/iconos/fase1-1_icon.png',
+    'assets/iconos/fase1-2_icon.png',
+    'assets/iconos/fase1-3_icon.png',
+    'assets/iconos/fase1-4_icon.png',
+    'assets/iconos/fase1-5_icon.png',
+    'assets/iconos/fase1-6_icon.png',
+    'assets/iconos/fase1-7_icon.png' 
+  ];
+  
+  iconosFase2 = [
+    'assets/iconos/fase2-1_icon.png',
+    'assets/iconos/fase2-2_icon.png',
+    'assets/iconos/fase2-3_icon.png',
+    'assets/iconos/fase2-4_icon.png',
+    'assets/iconos/fase2-5_icon.png',
+    'assets/iconos/fase2-6_icon.png',
+    'assets/iconos/fase2-7_icon.png',
+    'assets/iconos/fase2-8_icon.png'
+  ];
+  
+  iconosFase3 = [
+    'assets/iconos/fase3-1_icon.png',
+    'assets/iconos/fase3-2_icon.png',
+    'assets/iconos/fase3-3_icon.png',
+    'assets/iconos/fase3-4_icon.png',
+    'assets/iconos/fase3-5_icon.png',
+    'assets/iconos/fase3-6_icon.png',
+    'assets/iconos/fase3-7_icon.png'
+  ];
   constructor(
     private GruposService: GruposService,
     private unidadesService: UnidadesMedidaService,
     private calculoDeCostosService: CalculodecostosService,
     private router: Router
-  ) {}
+  ) {this.categoriaId = parseInt(localStorage.getItem('categoriaId') || '0', 10);}
 
   ngOnInit(): void {
     this.obtenerGrupos();
+    this.obtenerFasesPorGrupo();
   }
 
   obtenerGrupos(): void {
@@ -52,6 +86,17 @@ export class CategoriaComponent implements OnInit {
         console.error('Error al obtener los grupos:', error);
       }
     );
+  }
+  obtenerFasesPorGrupo(): void {
+    this.calculoDeCostosService.obtenerFasesPorGrupo(this.categoriaId).subscribe({
+      next: (data) => {
+        data.fases_produccion.forEach((fase: any) => console.log('Fase:', fase));
+        this.gruposF = this.groupFasesByProceso(data.fases_produccion);
+      },
+      error: (err) => {
+        console.error('Error fetching phases', err);
+      }
+    });
   }
 
   seleccionarCategoria(grupo: any): void {
@@ -192,13 +237,13 @@ export class CategoriaComponent implements OnInit {
     if (this.categoriaSeleccionada && this.categoriaSeleccionada.id) {
       localStorage.setItem('categoriaId', String(this.categoriaSeleccionada.id));
       Swal.fire({
-        icon: 'success',
+        icon:'success',
         title: 'Datos guardados',
         text: 'Los datos se han guardado correctamente.',
         confirmButtonText: 'Aceptar'
       }).then(() => {
-        this.closeModal(); 
-        this.router.navigate(['/asociado/fases-costos']);
+        this.mostrarContenido = true;
+        this.isModalOpen = false;
       });
     } else {
       console.error('ID de categoría inválido');
@@ -219,5 +264,47 @@ export class CategoriaComponent implements OnInit {
       }, 1000); 
     });
   }
+  verFasesCostos(): void {
+    this.mostrarContenido = true;
+    this.isModalOpen = false;
+  }
+
+
+  ocultarFasesCostos(): void {
+    this.mostrarContenido = false;
+    this.isModalOpen = true; 
+  }
+
+  groupFasesByProceso(fases: any[]): any[] {
+    const gruposF: any[] = []; 
+    fases.forEach((fase, index) => {
+      const procesoIndex = gruposF.findIndex(grupo => grupo.idGrupo === fase.idGrupo);
+      const iconos = this.asignarIconos(fase.idGrupo);
+      const icono = iconos[index % iconos.length]; 
+
+      if (procesoIndex === -1) {
+        gruposF.push({ idGrupo: fase.idGrupo, fases: [{ ...fase, icono }] });
+      } else {
+        gruposF[procesoIndex].fases.push({ ...fase, icono });
+      }
+    });
+    return gruposF;
+}
+asignarIconos(idGrupo: number): string[] {
+    switch (idGrupo) {
+      case 1:
+        return this.iconosFase1;
+      case 2:
+        return this.iconosFase2;
+      case 3:
+        return this.iconosFase3;
+      default:
+        return [];
+    }
+}
+verDetalleFase(id: number): void {
+  this.router.navigate(['/asociado/fases-costos', id]); 
+}
+
 }
 
--- src/app/components/asociado/fases-costos/fases-costos.component.ts
+++ src/app/components/asociado/fases-costos/fases-costos.component.ts
@@ -8,94 +8,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./fases-costos.component.css']
 })
 export class FasesCostosComponent implements OnInit {
-  grupos: any[] = []; 
-  categoriaId: number; 
-
-  iconosFase1 = [
-    'assets/iconos/fase1-1_icon.png',
-    'assets/iconos/fase1-2_icon.png',
-    'assets/iconos/fase1-3_icon.png',
-    'assets/iconos/fase1-4_icon.png',
-    'assets/iconos/fase1-5_icon.png',
-    'assets/iconos/fase1-6_icon.png',
-    'assets/iconos/fase1-7_icon.png' 
-  ];
   
-  iconosFase2 = [
-    'assets/iconos/fase2-1_icon.png',
-    'assets/iconos/fase2-2_icon.png',
-    'assets/iconos/fase2-3_icon.png',
-    'assets/iconos/fase2-4_icon.png',
-    'assets/iconos/fase2-5_icon.png',
-    'assets/iconos/fase2-6_icon.png',
-    'assets/iconos/fase2-7_icon.png',
-    'assets/iconos/fase2-8_icon.png'
-  ];
-  
-  iconosFase3 = [
-    'assets/iconos/fase3-1_icon.png',
-    'assets/iconos/fase3-2_icon.png',
-    'assets/iconos/fase3-3_icon.png',
-    'assets/iconos/fase3-4_icon.png',
-    'assets/iconos/fase3-5_icon.png',
-    'assets/iconos/fase3-6_icon.png',
-    'assets/iconos/fase3-7_icon.png'
-  ];
+ 
 
   constructor(
     private calculoCostosService: CalculodecostosService,
     private router: Router
   ) {
-    this.categoriaId = parseInt(localStorage.getItem('categoriaId') || '0', 10);
+    
   }
 
   ngOnInit(): void {
-    this.obtenerFasesPorGrupo();
+   
   }
 
-  obtenerFasesPorGrupo(): void {
-    this.calculoCostosService.obtenerFasesPorGrupo(this.categoriaId).subscribe({
-      next: (data) => {
-        data.fases_produccion.forEach((fase: any) => console.log('Fase:', fase));
-        this.grupos = this.groupFasesByProceso(data.fases_produccion);
-      },
-      error: (err) => {
-        console.error('Error fetching phases', err);
-      }
-    });
-  }
-
-  groupFasesByProceso(fases: any[]): any[] {
-    const grupos: any[] = []; 
-    fases.forEach((fase, index) => {
-      const procesoIndex = grupos.findIndex(grupo => grupo.idGrupo === fase.idGrupo);
-      const iconos = this.asignarIconos(fase.idGrupo);
-      const icono = iconos[index % iconos.length]; 
-
-      if (procesoIndex === -1) {
-        grupos.push({ idGrupo: fase.idGrupo, fases: [{ ...fase, icono }] });
-      } else {
-        grupos[procesoIndex].fases.push({ ...fase, icono });
-      }
-    });
-    return grupos;
-}
-
-asignarIconos(idGrupo: number): string[] {
-    switch (idGrupo) {
-      case 1:
-        return this.iconosFase1;
-      case 2:
-        return this.iconosFase2;
-      case 3:
-        return this.iconosFase3;
-      default:
-        return [];
-    }
-}
 
 
-  verDetalleFase(id: number): void {
-    this.router.navigate(['/asociado/fases-costos', id]); 
-  }
+
+ 
 }
