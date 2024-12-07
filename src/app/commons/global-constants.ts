afb5c5af0ba94150dbeed96d45f3e3450b8b7911
lionheartsq <lujogarpin78@gmail.com>
2024-12-07 08:58:22 -0500

    Add url
    Add url

--- src/app/commons/global-constants.ts
+++ src/app/commons/global-constants.ts
@@ -2,6 +2,8 @@ export class GlobalConstants {
 
   //  public static apiURL: string = "https://apianuc.hourclient.com";
 
+  // public static apiURL: string = "https://anucfloridablanca.api.senafloridablanca.com";
+
   //
   public static apiURL: string = "http://127.0.0.1:8000";
 
